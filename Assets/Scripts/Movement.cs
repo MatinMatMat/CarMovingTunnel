4c24235920e58a1bf1ea5c274250bcfbb6d9d2f1
DESKTOP-UO43OJN\MatMat <matinmatmat@gmail.com>
2023-12-23 09:49:26 -0800

    last min edit

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
@@ -13,23 +14,25 @@ public class Movement : MonoBehaviour
     [SerializeField]
     Transform car;
     [SerializeField]
-    float rotateSpeed = 40;
+    float  rotateSpeed;
 
 
-    Vector3 rotationRight = new Vector3(0, 40, 0);
-    Vector3 rotationLeft = new Vector3(0, -40, 0);
+ 
 
     void Update()
     {
+        Vector3 rotationRight = new Vector3(0, rotateSpeed, 0);
+        Vector3 rotationLeft = new Vector3(0, -rotateSpeed, 0);
+
         if (Input.GetKey(leftKey))
         {
-            Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRotationLeft);
+                Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotationLeft);
         }
         if (Input.GetKey(rightKey))
         {
-            Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRotationRight);
+                Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.deltaTime);
+                rb.MoveRotation(rb.rotation * deltaRotationRight);
         }
         if (Input.GetKey(forwardKey))
         {
